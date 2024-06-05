f8b1ab49b3c2c55a8d9b963aa1abe67a798e4a10
bikriira <bikriiradufasha@gmail.com>
2024-06-05 06:58:50 +0200

    [Fix]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,21 +5,27 @@ listed for a given subreddit.
 """
 
 
+from pprint import pprint
+
+
 def top_ten(subreddit):
     """
     Return top 10 posts of a given subreddit
     """
     import requests
 
-    hot_list = []
     headers = {'User-Agent': 'reddit-app/0.0.1'}
+    params = {'limit': 10}
     response = requests.get(
         f"https://www.reddit.com/r/{subreddit}/hot.json",
         headers=headers,
-        allow_redirects=False)
+        params=params,
+        allow_redirects=False
+    )
 
     if response.status_code == 200:
         data = response.json()
+        pprint(data)
         for count in range(0, 10):
             print(data["data"]["children"][count]["data"]["title"])
     else:
