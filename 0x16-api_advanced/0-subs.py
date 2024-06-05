a71999f9fd29482aa971b494116b1e058dc030ed
bikriira <bikriiradufasha@gmail.com>
2024-06-05 04:06:51 +0200

    [Achievement] do task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Find the number of subscribers (not active users, total subscribers)
+for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+"""
+
+
+def number_of_subscribers(subreddit):
+    import requests
+
+    headers = {'User-Agent': 'reddit-app/0.0.1'}
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json",
+        headers=headers,
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        data = response.json()
+        return (data["data"]["subscribers"])
+    else:
+        return 0
