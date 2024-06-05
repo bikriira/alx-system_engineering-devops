31de561f6467f5000fe3a47ecf54f4379fec666e
bikriira <bikriiradufasha@gmail.com>
2024-06-05 08:27:33 +0200

    [Try Fix] 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-"""prints the titles of the first 10 hot posts listed for a given subreddit.
-
-"""
+"""Module listing the title of top 10 hot post in the subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    '''Return the title of the first 10 hot posts
-    listed for a given subreddit'''
-    try:
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
-            (by /u/Large_Alternative_30)",
-        }
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if (response.status_code == 404):
-            print('None')
-            return 0
-        request = response.json().get('data').get('children')
-        for i in range(10):
-            print(request[i].get('data').get('title'))
-    except Exception:
+    """list the title of top 10 hot post in the subreddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    user = {'User-Agent': 'Test123'}
+    response = requests.get(url, headers=user, allow_redirects=False)
+    if response.status_code == 404:
         print('None')
-        return 0
+        return
+    for post in response.json().get('data').get('children'):
+        print(post.get('data').get('title'))
