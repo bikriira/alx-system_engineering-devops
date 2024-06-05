f85be577d75451a73e6dcd2a5358d251dfa8a698
bikriira <bikriiradufasha@gmail.com>
2024-06-05 04:10:27 +0200

    [Fix] add function documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,18 +5,23 @@ for a given subreddit.
 If an invalid subreddit is given, the function should return 0.
 """
 
+import requests
+
 
 def number_of_subscribers(subreddit):
-    import requests
-
+    """
+    Return the number of subscribers for a given subreddit.
+    If the subreddit is invalid, return 0.
+    """
     headers = {'User-Agent': 'reddit-app/0.0.1'}
     response = requests.get(
         f"https://www.reddit.com/r/{subreddit}/about.json",
         headers=headers,
         allow_redirects=False
     )
+
     if response.status_code == 200:
         data = response.json()
-        return (data["data"]["subscribers"])
+        return data["data"]["subscribers"]
     else:
         return 0
