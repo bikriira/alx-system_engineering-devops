bac65035592659dfbaddd1890d084e5bc8ccb1ff
bikriira <bikriiradufasha@gmail.com>
2024-06-05 04:50:59 +0200

    [debugging]

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,10 +14,6 @@ def number_of_subscribers(subreddit):
     If the subreddit is invalid, return 0.
     """
     headers = {'User-Agent': 'reddit-app/0.0.1'}
-    response = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/about.json",
-        headers=headers,
-        allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
