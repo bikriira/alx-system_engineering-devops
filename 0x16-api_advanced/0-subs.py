bb0e2a8f90d3ac5be2e4b455bdbfd1b1d2723fe5
bikriira <bikriiradufasha@gmail.com>
2024-06-05 09:23:45 +0200

    let try task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -22,6 +22,14 @@ def number_of_subscribers(subreddit):
 
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]  # No parentheses needed
+        return data["data"]["subscribers"]
     else:
         return 0
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        print(number_of_subscribers(sys.argv[1]))
+    else:
+        print("Usage: ./script_name.py subreddit_name")
