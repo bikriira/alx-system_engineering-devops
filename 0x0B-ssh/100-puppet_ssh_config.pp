c517b8e8090cfe53e662d273039931de389a6e06
bikriira <bikriiradufasha@gmail.com>
2024-06-12 00:15:05 +0200

    [Fix] file resource in puppet override the exisitng da

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,8 +2,12 @@
 # Just as in the previous configuration file task, we’d like you to set up your
 # client SSH configuration file so that you can connect to a server without typing a password
 
-file{'client configuration':
-  ensure  => 'present',
-  path    => '/etc/ssh/ssh_config',
-  content => 'host 100.24.244.146\n\tIdentityFile ~/.ssh/school\tPasswordAuthentication No',
+file_line { 'Declare_identity_file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'Turn_off_passwd_auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
