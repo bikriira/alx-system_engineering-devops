2e00b5ad677619a50022a4a2797abc6d916a152a
bikriira <bikriiradufasha@gmail.com>
2024-06-12 00:23:56 +0200

    [Try Fix]

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,19 @@
 # client SSH configuration file so that you can connect to a server without typing a password
 
 include stdlib
+file_line { 'Specify server address':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'host 100.24.244.146',
+  replace => true,
+}
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/holberton',
+  line    => '    IdentityFile ~/.ssh/school',
   replace => true,
 }
 
 file_line { 'Turn off passwd auth':
   path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
+  line    => '    PasswordAuthentication No',
   replace => true,
 }
