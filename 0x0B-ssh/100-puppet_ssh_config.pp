5db35e955ea911cd5c739720adf9d6365f687873
bikriira <bikriiradufasha@gmail.com>
2024-06-12 00:42:13 +0200

    [Try Fix]

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,8 +2,15 @@
 # Just as in the previous configuration file task, we’d like you to set up your
 # client SSH configuration file so that you can connect to a server without typing a password
 
-file { 'Tweak ssh config':
-  ensure  => 'present',
-  path    => '/etc/ssh/ssh_config',
-  content => "host 100.24.244.146\n\tIdentityFile ~/.ssh/school\n\tPasswordAuthentication No",
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
