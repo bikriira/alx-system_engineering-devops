cea2adbcc9cdd7e549fd32b2a0c413dab0815abc
bikriira <bikriiradufasha@gmail.com>
2024-06-12 00:20:41 +0200

    [Fix] include stdlib to use file_line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,12 +2,15 @@
 # Just as in the previous configuration file task, we’d like you to set up your
 # client SSH configuration file so that you can connect to a server without typing a password
 
-file_line { 'Declare_identity_file':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true,
 }
 
-file_line { 'Turn_off_passwd_auth':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
