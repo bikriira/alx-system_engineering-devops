be14fd891558bd58c9986eb37c0a448e41b4f126
bikriira <bikriiradufasha@gmail.com>
2024-06-12 00:25:12 +0200

    [Try Fix]

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # ASKED: Puppet to make changes to our configuration file.
 # Just as in the previous configuration file task, we’d like you to set up your
 # client SSH configuration file so that you can connect to a server without typing a password
